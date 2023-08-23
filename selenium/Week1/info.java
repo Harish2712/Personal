340112679892a43434824802f49a5b5a175998c5
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-08-23 10:52:39 +0530

    Create info.java

---
+++ selenium/Week1/info.java
@@ -0,0 +1,30 @@
+package week1day1;
+
+public class info {
+
+	public info() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int myNum = 10;
+		long cardNum = 890767896543l;
+		float test = 1.1f;
+		double test1 = 112.123;
+		boolean test2 = false;
+		char initial = 'm';
+		String name = "harish";
+		
+		System.out.println("Number is:"+myNum);
+		System.out.println("CardNumber is:"+cardNum);
+		System.out.println("Value is:"+test);
+		System.out.println("value1 is:"+test1);
+		System.out.println("value2 is:"+test2);
+		System.out.println("initial is:"+initial);
+		System.out.println("Name is:"+name);
+
+	}
+
+}
