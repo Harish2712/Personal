caa5b4fd533da2c2d0e4548bb688022e02614f8d
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-08-23 10:54:37 +0530

    Create Ifelsecondition.java

---
+++ selenium/Week1/Ifelsecondition.java
@@ -0,0 +1,29 @@
+package week1day1;
+
+public class Ifcondition {
+
+	public Ifcondition() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int num = 10;
+		
+		if(num>0)
+		{
+			System.out.println("Number is positive");
+		}
+		else if(num==0)
+		{
+			System.out.println("number is neutal");
+			
+		}
+		else
+		{
+			System.out.println("Number is negative");
+		}
+
+	}
+
+}
