3a1cf7524e831684ad9db122a17b6b0776b97bd9
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-08-23 10:57:22 +0530

    Create printodd.java

---
+++ selenium/Week1/printodd.java
@@ -0,0 +1,18 @@
+package week1day2;
+
+public class Printoddnumber {
+
+	public Printoddnumber() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	for(int i=1; i<=20; i++)
+	{
+		if(i%2==1) {
+			System.out.println(i);
+		}
+	}
+}
+}
