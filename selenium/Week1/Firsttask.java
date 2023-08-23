4b10c573bb0c93d5c3ff4e436debe09572fdba0b
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-08-23 10:49:41 +0530

    Create Firsttask.java

---
+++ selenium/Week1/Firsttask.java
@@ -0,0 +1,14 @@
+package week1day1;
+
+public class firsttask {
+
+	public firsttask() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("first task");
+
+	}
+}
