5ed68a1da36a2682cdd7763e638d71b8a35cb7e6
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-06 17:36:35 +0530

    Create MultipleLangauge.java

---
+++ selenium/Assignments/week3/Day1/Assignment3/MultipleLangauge.java
@@ -0,0 +1,11 @@
+package week3assignment;
+
+public abstract class MultipleLangauge{
+	
+	
+	public void python() {
+		System.out.println("Language is Python");
+	}
+	
+	public abstract void ruby();
+}
