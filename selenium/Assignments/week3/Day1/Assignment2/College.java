40b473ecf7af08793d0102eb33ec9079413e0326
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-06 17:33:23 +0530

    Create College.java

---
+++ selenium/Assignments/week3/Day1/Assignment2/College.java
@@ -0,0 +1,16 @@
+package org.college;
+
+public class College {
+
+	public void collegeName() {
+		System.out.println("College name is VIT");
+	}
+	
+	public void collegeCode() {
+		System.out.println("College Code is 112");
+	}
+	
+	public void collegeRank() {
+		System.out.println("College name is 2");
+	}
+}
