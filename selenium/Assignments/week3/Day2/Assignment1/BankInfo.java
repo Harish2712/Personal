dcf62df6776ea088a3f0543f769310540730e2bf
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-06 17:38:56 +0530

    Create BankInfo.java

---
+++ selenium/Assignments/week3/Day2/Assignment1/BankInfo.java
@@ -0,0 +1,17 @@
+package week3assignment;
+
+public class BankInfo {
+	
+	public void saving() {
+		System.out.println("Saving from BankInfo");
+	}
+	
+	public void fixed() {
+		System.out.println("fixed from BankInfo");
+	}
+	
+	public void deposit() {
+		System.out.println("deposit from BankInfo");
+	}
+
+}
