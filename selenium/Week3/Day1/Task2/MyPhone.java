e953e5fcfddb7dd634911d6c2ee802e0ce6c81dd
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-14 15:14:39 +0530

    Create MyPhone.java

---
+++ selenium/Week3/Day1/Task2/MyPhone.java
@@ -0,0 +1,16 @@
+package week3.Day1;
+
+public class MyPhone extends Iphone{
+
+	public static void main(String[] args) {
+
+		Iphone ip = new Iphone();
+		ip.makeCall();
+		ip.sendSms();
+		ip.startApp();
+		ip.increaseVolume();
+		ip.shutDown();
+		
+	}
+
+}
