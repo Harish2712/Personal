54e9edae60e352cf5ff4a3d3e64353a4b5124dc7
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-14 15:17:30 +0530

    Create ReverseTheString.java

---
+++ selenium/Week3/Day2/ReverseTheString.java
@@ -0,0 +1,15 @@
+package week3.day2;
+
+public class ReverseTheString {
+
+	public static void main(String[] args) {
+		
+		String input = "TestLeaf";
+		char[] name = input.toCharArray();
+		for(int i=name.length-1;i>=0;i--) {
+			System.out.println(name[i]);
+			name.toString();
+		}
+	}
+
+}
