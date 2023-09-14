fe1242bebe56e5a414a1c3a0bc4e220cea3022be
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-14 15:16:33 +0530

    Create Implementation.java

---
+++ selenium/Week3/Day2/Task1/Implementation.java
@@ -0,0 +1,11 @@
+package week3.day2;
+
+public class Implementation extends Polymorphism{
+
+	public static void main(String[] args) {
+
+		Implementation i = new Implementation();
+		i.startApp("firefox");
+	}
+
+}
