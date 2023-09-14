316c27fc0d64fa2e0835a28ac61b83539fd645d5
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-14 15:08:54 +0530

    Create IOS.java

---
+++ selenium/Week3/Day1/Task1/IOS.java
@@ -0,0 +1,16 @@
+package week3.Day1;
+
+public class Ios {
+
+	public void startApp() {
+		System.out.println("Starting app -- IOS class");
+	}
+	
+	public void increaseVolume() {
+		System.out.println("Increasing the volume -- IOS class");
+	}
+	
+	public void shutDown() {
+		System.out.println("Shuting down -- IOS class");
+	}
+}
