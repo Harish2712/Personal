a75a4ebd2317dd85b259554e3c3ff41fd3608582
Harish2712 <132554780+Harish2712@users.noreply.github.com>
2023-09-14 15:14:06 +0530

    Create Iphone.java

---
+++ selenium/Week3/Day1/Task2/Iphone.java
@@ -0,0 +1,13 @@
+package week3.Day1;
+
+public class Iphone extends Ios{
+
+	public void makeCall() {
+		
+		System.out.println("making a call -- from Iphone class");
+	}
+	
+	public void sendSms() {
+		System.out.println("sending a sms -- from iphone class");
+	}
+}
